b77c706ed28b14c8d06f5d3a6caf5da0dce2a4d6
이경원 <kyeong@igyeong-wons-MacBook-Pro.local>
2021-07-06 17:20:52 +0900

    mecab added

--- cheong_gaeguri/flask_for_HLT.py
+++ cheong_gaeguri/flask_for_HLT.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+
+from konlpy.tag import Mecab
 
 app = Flask(__name__)
 
@@ -7,9 +9,23 @@ def main():
     return render_template("main.html")
 
 @app.route('/mouth',methods = ['GET', 'POST'])
-def mouth():
-    return render_template("mouth.html")
+def mouth(): 
+    if request.method == 'GET':
+        print("gd")
+        return render_template("mouth.html")
 
+    if request.method == 'POST':
+        # sent = str(request.form['mouth'])
+        sent = request.form.get("mouth")
+        mecab = Mecab()
+        stnc_pos = mecab.pos(sent)
+
+        nouns = [n for n, tag in stnc_pos if tag in ["NNG","NNP","VV"] ]
+        print('ㅋㅋㅎㅎ 성공~')
+        print(nouns)
+        ###########################태깅 후 애니메이션 처리된 페이지 반환#######################
+        return render_template('mouth.html')
+        
 @app.route('/ear',methods = ['GET', 'POST'])
 def ear():
     return render_template("ear.html")
