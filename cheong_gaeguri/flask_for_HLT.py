7638c57eab5d05cdebba2123a9e70e8fc41a74c8
이경원 <kyeong@igyeong-wons-MacBook-Pro.local>
2021-07-07 13:25:57 +0900

    added with mecab

--- cheong_gaeguri/flask_for_HLT.py
+++ cheong_gaeguri/flask_for_HLT.py
@@ -11,7 +11,6 @@ def main():
 @app.route('/mouth',methods = ['GET', 'POST'])
 def mouth(): 
     if request.method == 'GET':
-        print("gd")
         return render_template("mouth.html")
 
     if request.method == 'POST':
@@ -21,10 +20,9 @@ def mouth():
         stnc_pos = mecab.pos(sent)
 
         nouns = [n for n, tag in stnc_pos if tag in ["NNG","NNP","VV"] ]
-        print('ㅋㅋㅎㅎ 성공~')
         print(nouns)
         ###########################태깅 후 애니메이션 처리된 페이지 반환#######################
-        return render_template('mouth.html')
+        return render_template('mouth.html' ,cont = nouns)
         
 @app.route('/ear',methods = ['GET', 'POST'])
 def ear():
--- cheong_gaeguri/static/py/get_animation.py
+++ cheong_gaeguri/static/py/get_animation.py
@@ -1,11 +1,21 @@
-from browser import document
-
-
-def convert(event):#애니메이션 생성 이벤트 리스너
-    ani_gen()
+# from browser import document
+# from konlpy.tag import Mecab
+import numpy as np
+# def convert(event):#애니메이션 생성 이벤트 리스너
+#     ani_gen()
 
 def ani_gen():#애니메이션 생성 
     print(int(document.getElementById("mouth").value) - 5)
+    # mecab = Mecab()
+    # stnc = getElementById("mouth").value
+    
+    # # stnc = '건너서 타면 됩니다.'
+
+    # stnc_pos = mecab.pos(stnc)
+
+    # nouns = [n for n, tag in stnc_pos if tag in ["NNG","NNP","VV"] ]
+
+    # print(nouns)
 
 
     
