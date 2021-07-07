9a8c2c7d6ae991aca966f32c10f892204fe0c440
이경원 <kyeong@igyeong-wons-MacBook-Pro.local>
2021-07-07 17:39:15 +0900

    template applied

--- cheong_gaeguri/flask_for_HLT.py
+++ cheong_gaeguri/flask_for_HLT.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def main():
-    return render_template("main.html")
+    return render_template("index.html")
 
 @app.route('/mouth',methods = ['GET', 'POST'])
 def mouth(): 
